c166cea684e764132885c0dc80be4254e8e57562
william thompson <wat4music@gmail.com>
2020-01-03 00:38:44 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -4,17 +4,17 @@
 //}
 //basicTeenager(13);
 function basicTeenager(age) {
-  if (age > 12 && age < 20)
+  if (age > 12 && age < 20) {
     return("You are a teenager!");
   basicTeenager(13)
 }
 function teenager(age) {
-  if (age > 12 && age < 20)
+  if (age > 12 && age < 20) {
     return("You are a teenager!")
+  }
   else {
     return("You are not a teenager")
   teenager(11)
-
 }
 
 function ageChecker(age) {
