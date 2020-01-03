75234b8ea512adba1f58af260322161b1f40eb01
william thompson <wat4music@gmail.com>
2020-01-03 00:27:25 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -7,8 +7,6 @@ function basicTeenager(age) {
   if (age > 12 && age < 20)
     return("You are a teenager!");
   basicTeenager(13)
-} else {
-    return("You are not a teenager")
 }
 
 function teenager(age) {
